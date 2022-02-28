5a3c3646d5ab969637f52ff7eb8035d9d746af64
elianamendez1 <elianamendez1809@gmail.com>
2022-02-27 20:20:07 -0500

    merged changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
+// // import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
+// // import data from './data/rickandmorty/rickandmorty.js';
 
+const btn = document.getElementById("showSecondPage")
+btn.addEventListener("click", () => {
+    document.getElementById("firstPage").style.display = "none";
+    document.getElementById("secondPage").style.display = "block";
+    document.querySelector("footer").style.display = "block";
+})
