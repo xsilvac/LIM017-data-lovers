12a8826147d55705ed691326846af14251796f62
ximena silva <ximenasilvacotrina11@gmail.com>
2022-03-07 08:23:49 -0500

    filters by director, producer and title

--- src/data.js
+++ src/data.js
@@ -1,11 +1,37 @@
-// estas funciones son de ejemplo
+export const filterDataDirector = (data, filterDirector) => {
+  const resultByDirector = data.filter(y => y.director === filterDirector);
+  return resultByDirector;
+};
 
-export const filterDataDirector = (data,condition) => {
-  return data[condition]
-  };
+export const filterDataProducer = (data, filterProducer) => {
+  const resultByProducer = data.filter(y => y.producer === filterProducer);
+  return resultByProducer;
+};
 
-const data = data.filter(filter);
+export const sortData = (data, sortBy, sortOrder) => {
+  const resultOrder = data.sort((a, b) => {
+    if (a[sortBy] < b[sortBy]) {
+      return -1;
+    }
+    else if (a[sortBy] > b[sortBy]) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  });
+  if (sortOrder === "titleDescending") {
+    return resultOrder.reverse();
+  }
+  return resultOrder, resultOrder.reverse();
+}
 
-let dataString = JSON.stringify(data);
-let parseData = JSON.parse(dataString);
+export const filterTitle = (data, x, y) => {
+  const resultFilterTitle = data.filter(item => item[x].toLowerCase().includes(y.toLowerCase()));
+  return resultFilterTitle;
+}
 
+    // export const filterDataCharacters = (data, filterCharacters) => {
+    //   const resultByCharacters= data.filter(y => y.people === filterCharacters);
+    //   return resultByCharacters;
+    // 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-// import { example } from './data.js';
+import { filterDataDirector, filterDataProducer, sortData, filterTitle} from './data.js';
 
 import data from './data/ghibli/ghibli.js';
 
 const hiddenFooter = document.querySelector("footer");
-    hiddenFooter.classList.add("oculto");
+hiddenFooter.classList.add("oculto");
 
 const hiddenNav = document.getElementById("nav");
-    hiddenNav.classList.add("oculto");
+hiddenNav.classList.add("oculto");
 
 const btn = document.getElementById("showSecondPage")
 btn.addEventListener("click", () => {
@@ -14,24 +14,118 @@ btn.addEventListener("click", () => {
     document.getElementById("secondPage").style.display = "block";
     hiddenFooter.classList.remove("oculto");
     hiddenNav.classList.remove("oculto");
-    });
+});
 
 let allMovies = data.films;
-const printFilms = document.getElementById("listOfFilms");
-for(let x of allMovies) {
-    printFilms.innerHTML +=
-    `<section class = "containerMovies" id="${x.id}">
-    <img src= "${x.poster}" class= "poster" >
+let printFilms = document.getElementById("listOfFilms");
+let moviesToShow = (x) => {
+    return `
+    <section class = "containerMovies" id="${x.id}">
+    <img src= "${x.poster}" class= "poster"/>
     <p id="title">${x.title}</p>
 </section>`
-}
+};
+
+const showInScreen = (y) => {
+    printFilms.innerHTML = "";
+    y.forEach((z) => {
+        printFilms.innerHTML += moviesToShow(z)
+    })
+};
+showInScreen(allMovies);
+
+const filterDirector = document.getElementById('filterDirector');
+filterDirector.addEventListener('change', (x) => {
+    const selectedDirector = filterDataDirector(allMovies, x.target.value);
+    showInScreen(selectedDirector);
+});
+
+const filterProducer = document.getElementById('filterProducer');
+filterProducer.addEventListener('change', (x) => {
+    const selectedProducer = filterDataProducer(allMovies, x.target.value);
+    showInScreen(selectedProducer);
+});
+
+// let ximena = document.getElementById("listOfFilms");
+
+// const showInScreen2 = (y) =>{
+//     printFilms.innerHTML ="";
+//     y.forEach((z) => {
+//         printFilms.innerHTML += moviesToShow(z)
+//     })
+// };
+// let prueba1 = data.films["people"];
+// console.log(prueba1);
+
+// const filterCharacters = document.getElementById('filterCharacters');
+// function peopleShow (contenedor, datatotal) {
+
+//     datatotal.forEach((people)=> {
+//         const detallesPersonajes = document.createElement("div");
+//         detallesPersonajes.className = "container-card-characters";
+//         detallesPersonajes.innerHTML =`
+//         <section class = "containerCharacters" id="${people.id}">
+//         <img src= "${people.img}" class= "poster"/>
+//         <p id="nameCharacters">${people.name}</p>
+//         <p id="ageCharacters">${people.age}</p>
+//         <p id="eyesCharacters">${people.eye_color}</p>
+//         <p id="hairCharacters">${people.hair_color}</p>
+//         <p id="specieCharacters">${people.specie}</p>
+//     </section>`
+//     contenedor.appendChild(detallesPersonajes);
+//     })
+// }
+// let ximena = document.getElementById('ximena');
+// function mostrarPersonajes() {
+//     ximena.style.display="block";
+//     showInScreen();
+//     document.querySelector("#secondPage").style.display = "none";
+    
+//    let totalPersonajes = filterDataCharacters(allMovies);
+
+//     peopleShow(filterCharacters,totalPersonajes);
+// }
+// showInScreen(mostrarPersonajes);
+// filterCharacters.addEventListener('change', (x) => {
+// const selectedCharacters = filterDataCharacters(allMovies, x.target.value);
+// showInScreen(selectedCharacters);
+
+// let charactersToShow = (people) =>{
+//     return`
+//     <section class = "containerCharacters" id="${people.id}">
+//     <img src= "${people.img}" class= "poster"/>
+//     <p id="nameCharacters">${people.name}</p>
+//     <p id="ageCharacters">${people.age}</p>
+//     <p id="eyesCharacters">${people.eye_color}</p>
+//     <p id="hairCharacters">${people.hair_color}</p>
+//     <p id="specieCharacters">${people.specie}</p>
+// </section>`
+// }
+// showInScreen(charactersToShow);
 
 
 
+// const characters = document.getElementById('characters');
+// characters.addEventListener('change', (x) => {
+//     const showInScreen2 = (y) =>{
+//         printFilms.innerHTML ="";
+//         y.forEach((z) => {
+//             printFilms.innerHTML += charactersToShow(z)
+//         })
+//     };
+//     showInScreen2(charactersToShow);
+//     const selectedCharacters = filterDataCharacters(allMovies, x.target.value);
+//     showInScreen2(selectedCharacters);
 
+// });
+const filterByOrder = document.getElementById('sectionOrder');
+filterByOrder.addEventListener('change', (x) => {
+    const selectedOrder = sortData(allMovies, x.target.value, x.target.value);
+    showInScreen(selectedOrder);
+});
 
-
-
-
-
-
+const navigationBar = document.querySelector('#navigationBar');
+navigationBar.addEventListener('keyup', () => {
+    const searchText = filterTitle(allMovies,'title', navigationBar.value.toLowerCase());
+    showInScreen(searchText);
+});
