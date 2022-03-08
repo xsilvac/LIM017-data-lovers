640c6a572fe1a44cfa7fb55d9cf84ca3e0fd82fb
elianamendez1 <elianamendez1809@gmail.com>
2022-03-08 10:14:38 -0500

    fetch

--- src/data.js
+++ src/data.js
@@ -1,11 +1,23 @@
 // estas funciones son de ejemplo
 
-export const filterDataDirector = (data,condition) => {
+/*export const filterDataDirector = (data,condition) => {
   return data[condition]
   };
 
 const data = data.filter(filter);
 
 let dataString = JSON.stringify(data);
-let parseData = JSON.parse(dataString);
+let parseData = JSON.parse(dataString);*/
+
+export const dataOrder = (data, order) => {
+  let titleSort = data.sort((a, z)=>{
+   return (a.title > z.title) ? 1:-1;
+   })
+   console.log(titleSort);
+   if(order === "ascendente"){titleSort}
+   if(order === "descendente"){titleSort.reverse()}
+
+   return titleSort;
+}
+
 
--- src/main.js
+++ src/main.js
@@ -1,32 +1,49 @@
 // import { example } from './data.js';
 
-import data from './data/ghibli/ghibli.js';
+// import data from './data/ghibli/ghibli.js';
 
-const hiddenFooter = document.querySelector("footer");
-    hiddenFooter.classList.add("oculto");
+async function obtenerDatos() {
+    const response = await fetch(`http://127.0.0.1:5500/src/data/ghibli/ghibli.json`);
+    const data = await response.json();
+    console.log(data.films);
 
-const hiddenNav = document.getElementById("nav");
-    hiddenNav.classList.add("oculto");
+    // const director = data.filter(data =>data.name ===)
 
-const btn = document.getElementById("showSecondPage")
-btn.addEventListener("click", () => {
-    document.getElementById("firstPage").style.display = "none";
-    document.getElementById("secondPage").style.display = "block";
-    hiddenFooter.classList.remove("oculto");
-    hiddenNav.classList.remove("oculto");
-    });
 
-let allMovies = data.films;
-const printFilms = document.getElementById("listOfFilms");
-for(let x of allMovies) {
-    printFilms.innerHTML +=
-    `<section class = "containerMovies" id="${x.id}">
-    <img src= "${x.poster}" class= "poster" >
-    <p id="title">${x.title}</p>
-</section>`
+
+  const hiddenFooter = document.querySelector("footer");
+     hiddenFooter.classList.add("oculto");
+
+ const hiddenNav = document.getElementById("nav");
+     hiddenNav.classList.add("oculto");
+
+ const btn = document.getElementById("showSecondPage")
+ btn.addEventListener("click", () => {
+     document.getElementById("firstPage").style.display = "none";
+     document.getElementById("secondPage").style.display = "block";
+     hiddenFooter.classList.remove("oculto");
+     hiddenNav.classList.remove("oculto");
+     });
+
+ let allMovies = data.films;
+ const printFilms = document.getElementById("listOfFilms");
+ for(let x of allMovies) {
+     printFilms.innerHTML +=
+     `<section class = "containerMovies" id="${x.id}">
+     <img src= "${x.poster}" class= "poster" >
+     <p id="title">${x.title}</p>
+ </section>`
+ }
+ 
+
+
+
+    // json.films.foreach((films) => {
+    //  console.log(films.poster);
+    //     })
 }
 
-
+obtenerDatos();
 
 
 
