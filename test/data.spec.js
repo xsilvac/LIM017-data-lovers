1f0e02f2272e5c5be63349afa818d51510347d40
elianamendez1 <elianamendez1809@gmail.com>
2022-03-10 15:53:15 -0500

    Test filterDirector

--- test/data.spec.js
+++ test/data.spec.js
@@ -9,8 +9,15 @@
  ]
 
  describe('filterDataDirector', () => {
-   it('devolver las animaciones según el director', () => {
-     expect(filterDataDirector(datatest, "director")).toEqual(datatest.arrayContainingdatatest("Hayao Miyazaki"));
+   it('siempre devuelve un array', () => {
+    expect(filterDataDirector(datatest, "director") instanceof Array).toBe(true);
+    //  expect(filterDataDirector(datatest, "director")).toEqual(datatest.arrayContainingdatatest("Hayao Miyazaki"));
+   });
+   it('devuelve las animaciones del director', () => {
+     const result = filterDataDirector(datatest, "Isao Takahata")
+
+     expect(result).toEqual([ { "title": "Grave of the Fireflies", "director": "Isao Takahata"}]);
+    //  expect(filterDataDirector(datatest, "director")).toEqual(datatest.arrayContainingdatatest("Hayao Miyazaki"));
    });
  });
 
